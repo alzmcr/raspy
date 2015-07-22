82e10019027173d6cde229694d468037bf77869c
Alessandro Mariani <amariani@expedia.com>
2015-07-22 19:37:37 +0100

    initial commit

---
+++ camera.py
@@ -0,0 +1,53 @@
+import time
+import picamera
+import numpy as np
+import io
+from scipy import misc
+from datetime import datetime
+
+class Camera():
+    def __init__(self, w=320, h=240):
+        self.camera = picamera.PiCamera(resolution=(w,h))
+        self.closed = self.camera.closed
+        self.resolution(w, h)
+        self.stream = io.BytesIO()
+
+    def snap(self, use_video_port=True):
+        if self.camera.closed:
+            self.camera = picamera.PiCamera(resolution=(self.w,self.h))
+
+        # take a snapshot
+        self.camera.capture(self.stream, format='yuv', use_video_port=use_video_port)
+        # save it to stream
+        self.stream.truncate(self.fw*self.fh)
+        self.stream.seek(0)
+        # transform image
+        image = np.fromstring(self.stream.getvalue(), dtype=np.uint8).reshape((self.fh, self.fw))[:self.h,:self.w]
+        # reset stream
+        self.stream.truncate(0)
+
+        return image
+
+    def savesnap(self, fname=None, use_video_port=True):
+        if fname is None:
+            fname = datetime.now().strftime('%Y%m%d_%H%M%S.%f.jpg')
+        # save to file
+        misc.imsave(fname, self.snap(use_video_port))
+
+    def resolution(self, w, h):
+        self.w, self.h = w, h
+        self.fw = (w + 31) // 32 * 32
+        self.fh = (h + 15) // 16 * 16
+        self.camera.resolution = (w, h)
+
+    def close(self):
+        self.camera.close()
+        self.closed = self.camera.closed
+
+    def __repr__(self):
+        return "Camera instance: resolution (%ix%i)" % (self.w, self.h)
+
+
+
+
+
---
+++ surveilance.py
@@ -0,0 +1,37 @@
+import camera
+import numpy as np
+from time import sleep, time
+
+class NightWatch():
+    def __init__(self):
+        self.camera = camera.Camera()
+        self.max_diff = 255 * (self.camera.w * self.camera.h)
+        self.tollerance = 0.05
+
+    def check_movement(self, snap1, snap2):
+        diff = np.abs((np.array(snap1,dtype=np.int16) - snap2)).sum()
+        change_perc = 1. * diff / self.max_diff
+        return change_perc
+
+    def vigilate(self, seconds=30, interval=1):
+        print "NightWatch: initialized"
+        start_time = time(); previous_snap = self.camera.snap()
+        while True:
+            # wait for the next cycle
+            sleep(interval)
+
+            if (time() - start_time) > seconds:
+                print "NightWatch: terminated"
+                break
+
+            current_snap = self.camera.snap()
+            if self.check_movement(current_snap, previous_snap) > self.tollerance:
+                print "WHITE WALKER COMING!!!!"
+
+            previous_snap = current_snap
+
+
+
+    def close(self):
+        self.camera.close()
+

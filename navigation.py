defbd3f203edad03a7880a8603ac35b2081da12c
Alessandro Mariani <amariani@expedia.com>
2015-08-14 19:09:56 +0100

    navigation with PWM

--- navigation.py
+++ navigation.py
@@ -7,43 +7,71 @@ from datetime import datetime
 if GPIO.getmode() == -1: GPIO.setmode(GPIO.BOARD)
 
 class Motor():
-    def __init__(self, name, gpio1, gpio2, gpio1_is_fw=True):
+    def __init__(self, name, gpio1, gpio2, gpio1_is_fw=True, use_pwm=True):
         self.name = name                # motor name
         self.gpio1 = gpio1              # gpio pin to move in one direction
         self.gpio2 = gpio2              # gpio pin to move in opposite direction
         self.gpio1_is_fw = gpio1_is_fw  # gpio1 is foward?
+        self.use_pwm = use_pwm          # use PWM to control speed of motor
         GPIO.setup(gpio1, GPIO.OUT)     # setup GPIO 1
         GPIO.setup(gpio2, GPIO.OUT)     # setup GPIO 1
+        if use_pwm:
+            # setting PWM
+            self.pwm1 = GPIO.PWM(gpio1, 50)
+            self.pwm2 = GPIO.PWM(gpio2, 50)
 
     # stop motor
     def stop(self):
-        GPIO.output(self.gpio1, False); GPIO.output(self.gpio2, False)
+        if self.use_pwm:
+            self.pwm1.stop(); self.pwm2.stop()
+        else:
+            GPIO.output(self.gpio1, False); GPIO.output(self.gpio2, False)
 
     # spin in one direction
-    def spin1(self, seconds):
-        GPIO.output(self.gpio1, False); GPIO.output(self.gpio2, True)
+    def spin1(self, seconds, power=100):
+        if self.use_pwm:
+            self.pwm1.stop(); self.pwm2.start(power)
+        else:
+            GPIO.output(self.gpio1, False); GPIO.output(self.gpio2, True)
         if seconds is not None: sleep(seconds); self.stop()
 
     # spin in opposite direction
-    def spin2(self, seconds):
-        GPIO.output(self.gpio1, True); GPIO.output(self.gpio2, False)
+    def spin2(self, seconds, power=100):
+        if self.use_pwm:
+            self.pwm1.start(power); self.pwm2.stop()
+        else:
+            GPIO.output(self.gpio1, True); GPIO.output(self.gpio2, False)
         if seconds is not None: sleep(seconds); self.stop()
 
-    def fw(self, seconds=None):
-        GPIO.output(self.gpio1, self.gpio1_is_fw); GPIO.output(self.gpio2, not self.gpio1_is_fw)
+    def fw(self, seconds=None, power=100):
+        if self.use_pwm:
+            if self.gpio1_is_fw: self.pwm1.start(power); self.pwm2.stop()
+            else: self.pwm1.stop(); self.pwm2.start(power)
+        else:
+            GPIO.output(self.gpio1, self.gpio1_is_fw); GPIO.output(self.gpio2, not self.gpio1_is_fw)
         if seconds is not None: sleep(seconds); self.stop()
 
-    def rw(self, seconds=None):
-        GPIO.output(self.gpio1, not self.gpio1_is_fw); GPIO.output(self.gpio2, self.gpio1_is_fw)
+    def rw(self, seconds=None, power=100):
+        if self.use_pwm:
+            if self.gpio1_is_fw: self.pwm1.stop(); self.pwm2.start(power)
+            else: self.pwm1.start(power); self.pwm2.stop()
+        else:
+            GPIO.output(self.gpio1, not self.gpio1_is_fw); GPIO.output(self.gpio2, self.gpio1_is_fw)
         if seconds is not None: sleep(seconds); self.stop()
 
 class Rover():
-    def __init__(self, m1, m2, jib=None):
+    def __init__(self, m1, m2, jib=None, verbose=False):
         self.m1 = m1
         self.m2 = m2
         self.acc = Accel()
         self.motorlog = []
         self.jib = jib
+        # CURRENT STATE
+        self.state = 'stop'
+        self.power = 0
+        self.steerpower = 0
+        # DEBUG
+        self.verbose = verbose
 
     def keycontrol(self):
         mapping = {
@@ -61,36 +89,96 @@ class Rover():
 
     # DIRECTION
     def stop(self):
-        self.m1.stop(); self.m2.stop()
-        self.motorlog.append([time(),'stop'])
+        self.m1.stop(); self.m2.stop(); self.state = 'stop'; self.power = 0
+        self.motorlog.append([time(),'stop',0,0])
+        if self.verbose: print "stop | m1: %i | m2: %i" % (0,0)
 
     def fw(self, seconds=None):
-        self.m1.fw(); self.m2.fw()
-        self.motorlog.append([time(),'fw'])
-        if seconds is not None:
-            sleep(seconds); self.stop()
+        if m1.use_pwm and m2.use_pwm:
+            if self.state == 'rw': self.power = 0
+            self.power = min(100, max(30, self.power + 10)); self.steerpower = 0
+            self._fw(seconds, power=self.power)
+        else: self._fw(seconds)
 
     def rw(self, seconds=None):
-        self.m1.rw(); self.m2.rw()
-        self.motorlog.append([time(),'rw'])
+        if m1.use_pwm and m2.use_pwm:
+            if self.state == 'fw': self.power = 0
+            self.power = min(100, max(30, self.power + 10)); self.steerpower = 0
+            self._rw(seconds, power=self.power)
+        else: self._fw(seconds)
+
+    def left(self, seconds=None):
+        if m1.use_pwm and m2.use_pwm:
+            self.steerpower = min(100, max(30, self.steerpower + 10))
+            self._left(seconds, steerpower=self.steerpower)
+        else: self._left(seconds)
+
+    def right(self, seconds=None):
+        if m1.use_pwm and m2.use_pwm:
+            self.steerpower = min(100, max(30, self.steerpower + 10))
+            self._right(seconds, steerpower=self.steerpower)
+        else: self._right(seconds)
+
+    def _fw(self, seconds=None, power=100):
+        self.m1.fw(power=power); self.m2.fw(power=power); self.state = 'fw'
+        self.motorlog.append([time(),'fw',power,power])
+        if self.verbose: print "fw | m1.fw: %i | m2.fw: %i" % (power,power)
         if seconds is not None:
             sleep(seconds); self.stop()
 
-    def left(self, seconds=None, motor=None):
-        if motor is None: self.m1.rw(); self.m2.fw()
-        elif motor == 1: self.m1.rw()
-        elif motor == 2: self.m2.fw()
-        else: raise Exception('Wrong motor specified: %s' %motor)
-        self.motorlog.append([time(),'left'])
+    def _rw(self, seconds=None, power=100):
+        self.m1.rw(power=power); self.m2.rw(power=power); self.state = 'rw'
+        self.motorlog.append([time(),'rw',power,power])
+        if self.verbose: print "rw | m1.rw: %i | m2.rw %i" % (power,power)
         if seconds is not None:
             sleep(seconds); self.stop()
 
-    def right(self, seconds=None, motor=None):
-        if motor is None: self.m1.fw(); self.m2.rw()
-        elif motor == 1: self.m1.fw()
-        elif motor == 2: self.m2.rw()
-        else: raise Exception('Wrong motor specified: %s' %motor)
-        self.motorlog.append([time(),'right'])
+    def _left(self, seconds=None, steerpower=100):
+        if m1.use_pwm and m2.use_pwm:
+            # if the rover is not moving, rotating power is forced to 100
+            if self.state == 'stop': steerpower = 100; self.power = 100; self.state ='fw'
+            m2power = self.power
+            self.m2.rw(power=m2power) if self.state == 'rw' else self.m2.fw(power=m2power)
+            if steerpower >=50:
+                # STRONG TURN (inverted direction on motors)
+                m1power = ((steerpower-50)*2/100.)*self.power
+                self.m1.rw(power=m1power) if self.state == 'fw' else self.m1.fw(power=m1power)
+                #self.m2.fw(power=(power-50)*2) if self.state == 'rw' else self.m2.rw(power=(power-50)*2)
+            else:
+                # LIGHT TURN  (same direction, but slower motors)
+                m1power = ((50-steerpower)*2/100.)*self.power
+                self.m1.fw(power=m1power) if self.state == 'fw' else self.m1.rw(power=m1power)
+                #self.m2.rw(power=(50-power)*2) if self.state == 'rw' else self.m2.fw(power=(50-power)*2)
+        else:
+            self.m1.rw(power=self.power); self.m2.fw(power=self.power)
+            m1power, m2power = self.power, self.power
+        # log motor
+        self.motorlog.append([time(),'left',m1power,m2power])
+        if seconds is not None:
+            sleep(seconds); self.stop()
+
+    def _right(self, seconds=None, steerpower=100):
+        if m1.use_pwm and m2.use_pwm:
+            # if the rover is not moving, rotating power is forced to 100
+            if self.state == 'stop': steerpower = 100; self.power = 100; self.state ='fw'
+            m1power = self.power
+            self.m1.fw(power=m1power) if self.state == 'fw' else self.m1.rw(power=m1power)
+            if steerpower >=50:
+                # STRONG TURN (inverted direction on motors)
+                m2power = ((steerpower-50)*2/100.)*self.power
+                self.m2.fw(power=m2power) if self.state == 'rw' else self.m2.rw(power=m2power)
+                #self.m2.rw(power=(power-50)*2) if self.state == 'fw' else self.m2.fw(power=(power-50)*2)
+            else:
+                # LIGHT TURN  (same direction, but slower motors)
+                m2power = ((50-steerpower)*2/100.)*self.power
+                self.m2.fw(power=m2power) if self.state == 'fw' else self.m2.rw(power=m2power)
+                #self.m2.fw(power=(50-power)*2) if self.state == 'fw' else self.m2.rw(power=(50-power)*2)
+        else:
+            self.m1.fw(power=self.power); self.m2.rw(power=self.power)
+            m1power, m2power = self.power, self.power
+        # log motor
+        self.motorlog.append([time(),'right',m1power,m2power])
+
         if seconds is not None:
             sleep(seconds); self.stop()
 
